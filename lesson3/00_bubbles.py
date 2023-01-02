f5410686d7f26b4df3e8674a57f30b8dc9c90079
Дмитрий <drallmar@mail.ru>
2023-01-02 17:39:22 +0300

    gg333

--- lesson3/00_bubbles.py
+++ lesson3/00_bubbles.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
---
+++ lesson_002/probe.py
@@ -0,0 +1,3 @@
+zoo_pets = ['lion', 'skunk', 'elephant', 'horse', 'lion', 'skunk', 'elephant', 'horse',]
+for i, animal in enumerate(zoo_pets, 1):
+    print(i, animal)
